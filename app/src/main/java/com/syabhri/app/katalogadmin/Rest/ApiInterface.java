d909173c7767655a0ead0939e49d30ed95228b65
Muhammad Syabhri Mustafa <syabhri@gmail.com>
2018-12-14 15:30:10 +0700

    finished ApiInterface

--- app/src/main/java/com/syabhri/app/katalogadmin/Rest/ApiInterface.java
+++ app/src/main/java/com/syabhri/app/katalogadmin/Rest/ApiInterface.java
@@ -1,6 +1,8 @@
 package com.syabhri.app.katalogadmin.Rest;
 
+import com.syabhri.app.katalogadmin.Model.GetMobil;
 import com.syabhri.app.katalogadmin.Model.GetReview;
+import com.syabhri.app.katalogadmin.Model.GetUser;
 import com.syabhri.app.katalogadmin.Model.PostPutDelReview;
 
 import retrofit2.Call;
@@ -33,6 +35,62 @@ public interface ApiInterface {
     @HTTP(method = "DELETE", path = "review/user", hasBody = true)
     Call<PostPutDelReview> deleteReview(@Field("id_review") String idReview);
 
+    /********* Mobil *********/
+    @GET("mobil/all")
+    Call<GetMobil> getMobil();
 
+    @FormUrlEncoded
+    @POST("mobil/all")
+    Call<GetMobil> postMobil(
+            @Field("id_mobil") String idMobil,
+            @Field("tahun_mobil") String tahunMobil,
+            @Field("merek_mobil") String merekMobil,
+            @Field("photo_url") String photoUrl,
+            @Field("action") String action
+    );
+
+    @FormUrlEncoded
+    @POST("mobil/all")
+    Call<GetMobil> putMobil(
+            @Field("id_mobil") String idMobil,
+            @Field("tahun_mobil") String tahunMobil,
+            @Field("merek_mobil") String merekMobil,
+            @Field("photo_url") String photoUrl,
+            @Field("action") String action
+    );
+
+    @FormUrlEncoded
+    @POST("mobil/all")
+    Call<GetMobil> deleteMobil(
+            @Field("id_mobil") String idMobil,
+            @Field("action") String action);
+
+    /********* user *********/
+    @GET("user/all")
+    Call<GetUser> getUser();
+
+    @FormUrlEncoded
+    @POST("user/all")
+    Call<GetUser> postUser(
+            @Field("id_user") String idUser,
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("action") String action
+    );
+
+    @FormUrlEncoded
+    @POST("user/all")
+    Call<GetUser> putUser(
+            @Field("id_user") String idUser,
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("action") String action
+    );
+
+    @FormUrlEncoded
+    @POST("user/all")
+    Call<GetUser> deleteUser(
+            @Field("id_user") String idUser,
+            @Field("action") String action);
 
 }
